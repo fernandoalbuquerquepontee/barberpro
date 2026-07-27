b532a30c0e3b84f5ba54aefcbfb043dfc2bc3e2d
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-07-27 13:10:26 -0300

    feat: add to create appointment

---
+++ src/usecases/CreateAppointment.ts
@@ -0,0 +1,41 @@
+import type { Status } from "@/generated/prisma";
+import { prisma } from "@/lib/db";
+
+interface OutputDto {
+  id: string;
+  serviceId: string;
+  userId: string;
+  barberId: string;
+  date: Date;
+  status: Status;
+}
+
+interface InputDto {
+  serviceId: string;
+  userId: string;
+  barberId: string;
+  date: Date;
+  status: Status;
+}
+
+export class CreateAppointment {
+  async execute(input: InputDto): Promise<OutputDto> {
+    const now = new Date();
+
+    if (input.date < now) {
+      throw new Error("Cannot create an appointment in the past");
+    }
+
+    const appointment = await prisma.appointment.create({
+      data: {
+        serviceId: input.serviceId,
+        userId: input.userId,
+        barberId: input.barberId,
+        date: input.date,
+        status: input.status,
+      },
+    });
+
+    return appointment;
+  }
+}
