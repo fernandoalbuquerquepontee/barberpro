c19c68272319f48d9e7df633cad1e1e4bce1510b
Fernando Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-09-18 10:13:03 -0300

    feat: add new CORS URL for barber production site

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,6 +17,7 @@ export const auth = betterAuth({
     "http://127.0.0.1:8080",
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://barber-pro-umber.vercel.app",
   ],
   socialProviders: {
     google: {
