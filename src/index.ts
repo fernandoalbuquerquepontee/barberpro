02dbc1096cf62a7dde121674b8d08871f608b2d4
Fernando Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-09-18 01:14:37 -0300

    feat: allow app to listen on all network interfaces by specifying host

--- src/index.ts
+++ src/index.ts
@@ -115,7 +115,10 @@ app.route({
 });
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 8080 });
+  await app.listen({
+    port: Number(process.env.PORT) || 8080,
+    host: "0.0.0.0",
+  });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
